2c30f9b25812aca90300b45ed59deb5ac98f7321
noamBercovici <33417867+noamBercovici@users.noreply.github.com>
2017-11-17 13:14:05 +0100

    Update rootApp.js

--- www/js/rootApp.js
+++ www/js/rootApp.js
@@ -26,7 +26,7 @@ alert('config begin');
 alert('config end');
 });  // End rootApp config
 
-rootApp.run(function($rootScope,$location,$timeout,MapFactory){alert('run begin');});
+rootApp.run(function($rootScope,$location,$timeout,MapFactory,DataFactory){alert('run begin');});
 //
 //   APP INIT
 //
