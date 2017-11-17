ef898b4ba812226236159bc161b7d926b0d208b5
noamBercovici <noam.bercovici@gmail.com>
2017-11-17 11:35:27 +0100

    debug file for angular

--- www/js/deviceready.js
+++ www/js/deviceready.js
@@ -1,16 +1,6 @@
 // Adding or removing app boot HTML messages
 // example: createElement('myID','OK!','text-ok','fa fa-check fa-2x')
 //
-function messageCreate(id, text, text_class, icon_class) {
-      document.write('<div id="'+id+'" class="deviceMessage '+text_class+'"><div class="deviceMessageInner"><i class="'+icon_class+'" style="margin:5px 0 15px 0;"></i><br>'+text+'</div></div>');
-}
-
-function messageDelete(id) {
-      let element = document.getElementById(id);
-      element.outerHTML = "";
-      delete element;
-}
-
 var wasOffline = false;
 
 // =============== CORDOVA CONFIGURATION ===============
@@ -18,7 +8,6 @@ var wasOffline = false;
 var cordovaEvents = {
   // Application Constructor
   initialize: function() {
-
       console.log("Cordova init.");
       document.addEventListener('deviceready', this.onDeviceReady, false);
       document.addEventListener("resume", this.onResume, false);
@@ -30,187 +19,78 @@ var cordovaEvents = {
   // deviceready
   //
   onDeviceReady: function() {
+      console.warn("dito-m-anliegen: device ready.");
 
-                        console.warn("dito-m-anliegen: device ready.");
+      // Update camera plugin options from template in properties.js
+      // This is because Camera methods are not yet available when
+      // properties are initialized.
+      GLOBAL_ONTO.init.photoOptions.camera.sourceType =
+        Camera.PictureSourceType[GLOBAL_ONTO.init.photoOptions.camera.sourceType];
+      GLOBAL_ONTO.init.photoOptions.camera.destinationType =
+        Camera.DestinationType[GLOBAL_ONTO.init.photoOptions.camera.destinationType];
+      GLOBAL_ONTO.init.photoOptions.camera.mediaType =
+        Camera.MediaType[GLOBAL_ONTO.init.photoOptions.camera.mediaType];
+      GLOBAL_ONTO.init.photoOptions.camera.encodingType =
+        Camera.EncodingType[GLOBAL_ONTO.init.photoOptions.camera.encodingType];
+      //
+      GLOBAL_ONTO.init.photoOptions.gallery.sourceType =
+        Camera.PictureSourceType[GLOBAL_ONTO.init.photoOptions.gallery.sourceType];
+      GLOBAL_ONTO.init.photoOptions.gallery.destinationType =
+        Camera.DestinationType[GLOBAL_ONTO.init.photoOptions.gallery.destinationType];
+      GLOBAL_ONTO.init.photoOptions.gallery.mediaType =
+        Camera.MediaType[GLOBAL_ONTO.init.photoOptions.gallery.mediaType];
+      //
+      GLOBAL_ONTO.init.photoOptions.cleanup.sourceType =
+        Camera.PictureSourceType[GLOBAL_ONTO.init.photoOptions.cleanup.sourceType];
+      GLOBAL_ONTO.init.photoOptions.cleanup.destinationType =
+        Camera.DestinationType[GLOBAL_ONTO.init.photoOptions.cleanup.destinationType];
 
+      // =============== APP LAUNCHER ===============
 
-                        // XXX Style iOS bugs?
-                        // oder https://www.npmjs.com/package/cordova-plugin-statusbar
+      // Check online / offline status
+      // Using cordova plujgin network-information
+      GLOBAL_ONTO.init.debug && console.log("isOnline", navigator.connection);
+      angular.bootstrap(document, ['rootApp']);
+      var elt = document.getElementById('outer-message');
+      if(elt != null)
+        elt.setAttribute('style', 'display:block;');
 
-                        // if (parseFloat(window.device.version) === 7.0) {
-                        //          document.body.style.marginTop = "20px";
-                        //    }
-
-                        // Update camera plugin options from template in properties.js
-                        // This is because Camera methods are not yet available when properties are initialized
-                        GLOBAL_ONTO.init.photoOptions.camera.sourceType = Camera.PictureSourceType[GLOBAL_ONTO.init.photoOptions.camera.sourceType];
-                        GLOBAL_ONTO.init.photoOptions.camera.destinationType = Camera.DestinationType[GLOBAL_ONTO.init.photoOptions.camera.destinationType];
-                        GLOBAL_ONTO.init.photoOptions.camera.mediaType = Camera.MediaType[GLOBAL_ONTO.init.photoOptions.camera.mediaType];
-                        GLOBAL_ONTO.init.photoOptions.camera.encodingType = Camera.EncodingType[GLOBAL_ONTO.init.photoOptions.camera.encodingType];
-                        //
-                        GLOBAL_ONTO.init.photoOptions.gallery.sourceType = Camera.PictureSourceType[GLOBAL_ONTO.init.photoOptions.gallery.sourceType];
-                        GLOBAL_ONTO.init.photoOptions.gallery.destinationType = Camera.DestinationType[GLOBAL_ONTO.init.photoOptions.gallery.destinationType];
-                        GLOBAL_ONTO.init.photoOptions.gallery.mediaType = Camera.MediaType[GLOBAL_ONTO.init.photoOptions.gallery.mediaType];
-                        //
-                        GLOBAL_ONTO.init.photoOptions.cleanup.sourceType = Camera.PictureSourceType[GLOBAL_ONTO.init.photoOptions.cleanup.sourceType];
-                        GLOBAL_ONTO.init.photoOptions.cleanup.destinationType = Camera.DestinationType[GLOBAL_ONTO.init.photoOptions.cleanup.destinationType];
-
-                        // =============== APP LAUNCHER ===============
-
-                        // NETWORK DETECTION
-                        // Check online / offline status
-                        // Using cordova plujgin network-information
-
-                        function deviceOnline() {
-                              var networkState = navigator.connection.type;
-                              switch (networkState) {
-                                case "unknown": case "none":
-                                  var status = false;
-                                break;
-                                default: var status = true;  break;
-                              } // End switch
-                              return status;
-                         }
-
-                        console.warn("Online:", deviceOnline());
-                        console.log("XXX", navigator.connection);
-
-                        if (deviceOnline()) {
-                              // LAUNCH APP
-                              console.log('deviceready.js: Initializing Angular App');
-                              // Angular needs to be initialized after deviceready
-                              angular.bootstrap(document, ['rootApp']);
-                         } else {
-
-                              messageCreate("netMsg", text_error, "text-device-error", "fa fa-spin fa-spinner fa-2x");
-
-                              // INTERVAL CHECK LOOP
-
-                              let onlineCheck = setInterval(function(){
-
-                                GLOBAL_ONTO.init.debug && console.log('Waiting for net connection ... ');
-
-                                      if (deviceOnline()) {
-
-                                            console.warn('Network connection found.');
-                                            messageDelete("netMsg");
-
-                                            clearInterval(onlineCheck);
-
-                                            // wait a second for full network coming up
-
-                                            setTimeout(function() {
-                                                // LAUNCH APP
-                                                console.log('deviceready.js: Initializing Angular App');
-
-                                                // Angular needs to be initialized after deviceready
-                                                angular.bootstrap(document, ['rootApp']);
-
-                                              },1000);  // End timeout
-
-                                          } // End if online
-
-                                }, 2000 ); // End interval
-
-                        }  // End device offline
-
-                      },  // End devicereday
+  },  // End devicereday
 
   onPause: function() {
-                          console.log("devready: PASUE");
-                      },
-
+    GLOBAL_ONTO.init.debug && console.log("devready: PASUE");
+  },
   onResume: function() {
-                          console.log("devready: RESUME");
-                       },
-
-  onBackKeyDown: function()
-                      {
-                          console.log("devready: BACK-BUTTON");
-                      },
+    GLOBAL_ONTO.init.debug && console.log("devready: RESUME");
+  },
+  onBackKeyDown: function(){
+    GLOBAL_ONTO.init.debug && console.log("devready: BACK-BUTTON");
+  },
   onOffline : function() {
     wasOffline = true;
-    navigator.notification.alert('Sie haben keine Internet-Verbindung!', function(){}, 'Anliegenmanagement');
+    navigator.notification.alert('Sie haben keine Internet-Verbindung!',
+        function(){}, 'Anliegenmanagement');
   },
   onOnline : function() {
     if(wasOffline)
-    navigator.notification.alert('Gut! Sie sind wieder online.', function(){}, 'Anliegenmanagement');
-
+      navigator.notification.alert('Gut! Sie sind wieder online.',
+        function(){}, 'Anliegenmanagement');
   },
 
 };  // End var cordova
 
-
-
 // =============== APP ENTRY ===============
-
 // Helper fork to support browser debugging
-
 if (window.cordova) {
-
-  GLOBAL_ONTO.init.debug && console.log("Cordova detected.")
-  cordovaEvents.initialize();
-
-
+    GLOBAL_ONTO.init.debug && console.log("Cordova detected.");
+    cordovaEvents.initialize();
 } else {  // browser debugging mode
-
-       GLOBAL_ONTO.init.debug && console.log("Browser detected.")
-       // if (document.createEvent) { element.dispatchEvent('deviceready'); }
-
-      //  function deviceOnline() { return navigator.onLine }
-
-       console.warn("Online:", navigator.onLine);
-
-      //  if (deviceOnline()) {
-             // LAUNCH APP
-             console.log('deviceready.js: Initializing Angular App');
-             // Angular needs to be initialized after deviceready
-             angular.bootstrap(document, ['rootApp']);
-// var list = document.getElementsByClassName('alert-message');
-// for (var i = 0, length= list.length ; i < length ; i++) {
-//   list[i].style.display = 'block';
-//
-// }
-      //   } else {  // device offline
-       //
-      //        messageCreate("netMsg", text_error, "text-device-error", "fa fa-spin fa-spinner fa-2x");
-       //
-      //        // INTERVAL CHECK LOOP
-       //
-      //        let onlineCheck = setInterval(function(){
-       //
-      //          GLOBAL_ONTO.init.debug && console.log('Waiting for net connection ... ');
-       //
-      //                if (deviceOnline()) {
-      //                      console.warn('Network connection found.');
-      //                      messageDelete("netMsg");
-      //                      clearInterval(onlineCheck);
-      //                   // wait a second for full network coming up
-      //                   console.log('deviceready.js: Initializing Angular App');
-      //                   // Angular needs to be initialized after deviceready
-      //                    angular.bootstrap(document, ['rootApp']);
-       //
-      //              } // End if online
-       //
-      //          }, 2000 ); // End interval
-       //
-      //  }  // End device offline
+    GLOBAL_ONTO.init.debug && console.log("Browser detected.");
+    console.warn("Online:", navigator.onLine);
+    // Angular needs to be initialized after deviceready
+    angular.bootstrap(document, ['rootApp']);
+    var elt = document.getElementById('outer-message');
+    if(elt != null)
+      elt.setAttribute('style', 'display:block;');
 
 }  // End browser debug mode
-
-
-
-
-// // Eaxmple update DOM on a Received Event
-// receivedEvent: function(id) {
-//     var parentElement = document.getElementById(id);
-//     var listeningElement = parentElement.querySelector('.listening');
-//     var receivedElement = parentElement.querySelector('.received');
-//
-//     listeningElement.setAttribute('style', 'display:none;');
-//     receivedElement.setAttribute('style', 'display:block;');
-//
-//     console.log('Received Event: ' + id);
-//     console.log("Avaliable plugins:");
-//     console.log(navigator.camera);
-// },
