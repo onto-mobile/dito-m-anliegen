a4abf57e227dd0f943d46952ba045783b48ea647
noamBercovici <noam.bercovici@gmail.com>
2017-11-21 10:46:51 +0100

    reload app after being offline

--- www/js/controller/howTo.js
+++ www/js/controller/howTo.js
@@ -1,33 +1,23 @@
 rootApp.controller('howToCtrl', function($scope,$rootScope, $http) {
+  $scope.loadContent = function() {
     $http({
-        method: 	'GET',
-        url:			GLOBAL_ONTO.init.url_faq()
-      }).then(
-          function onSucc(response){
-                  $scope.contentValue = response.data;
-                  return true;
-          },
-
-          function onFail(response){
-                  return false;
-          }
-    );
-    $scope.$on('updateModel', function (event, data) {
-      	$timeout(function(){
-          $http({
-              method: 	'GET',
-              url:			GLOBAL_ONTO.init.url_faq()
-            }).then(
-                function onSucc(response){
-                        $scope.contentValue = response.data;
-                        return true;
-                },
-
-                function onFail(response){
-                        return false;
-                }
-          );
-          $scope.$apply;
-        });
-    });
+         method: 	'GET',
+         url:			GLOBAL_ONTO.init.url_faq()
+       }).then(
+           function onSucc(response){
+                   $scope.contentValue = response.data;
+                   return true;
+           },
+           function onFail(response){
+                   return false;
+           }
+     );
+  };
+  $scope.$on('updateModel', function (event, data) {
+    	$timeout(function(){
+        $scope.loadContent();
+        $scope.$apply;
+      });
+  });
+  $scope.loadContent();
 });
