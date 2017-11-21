157b8755399c6efa953bdda3a7a5dd6c44bdfeb3
noamBercovici <noam.bercovici@gmail.com>
2017-11-21 09:19:02 +0100

    reload app after being offline

--- www/js/controller/main.js
+++ www/js/controller/main.js
@@ -232,7 +232,7 @@ $scope.$watch('online', function(newStatus) {
 			console.log("back on line");
 			$rootScope.baseMap._resetView($rootScope.baseMap.getCenter(), $rootScope.baseMap.getZoom(), true);
 			NetworkService.getCategories().then(function(cat){
-				$rootScope.listOfCategories = receivedCategories;
+				$rootScope.listOfCategories = cat;
 				$rootScope.apply;
 			});
 			$scope.reloadContent();
